e4a0e691e5706bb1392bd54d5f447ccb22b7ae2f
Виноградов Кирилл <xam9lk60rus@gmail.com>
2019-07-12 15:41:10 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+﻿module.exports = {
+  PORT: process.env.PORT || 3000,
+  MONGO_URL: 'mongodb://localhost:27017/webjournal'
+};
---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+﻿/* eslint-disable node/no-unpublished-require */
+const {
+  watch, src, dest, series, parallel
+} = require('gulp');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const cleanHTML = require('gulp-htmlmin');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const plumber = require('gulp-plumber');
+const nodemon = require('gulp-nodemon');
+const browserSync = require('browser-sync');
+/* eslint-enable node/no-unpublished-require */
+
+function sassTranslate() {
+  return src('dev/scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['defaults'],
+        cascade: true
+      })
+    )
+    .pipe(cssnano())
+    .pipe(dest('./public/stylesheets'))
+    .pipe(browserSync.stream());
+}
+
+function pugTranslate() {
+  return src('dev/*.pug')
+    .pipe(plumber())
+    .pipe(pug())
+    .pipe(cleanHTML())
+    .pipe(dest('views'));
+}
+
+function nodemonTask(done) {
+  let started = false;
+  nodemon({
+    script: 'app.js',
+    ext: 'js pug',
+    env: { NODE_ENV: 'development' },
+    done
+  })
+    .on('start', () => {
+      if (!started) {
+        done();
+        started = true;
+      }
+    })
+    .on('restart', () => {
+      setTimeout(() => {
+        browserSync.reload({
+          stream: false
+        });
+      }, 100);
+    });
+}
+
+function browserSyncTask(done) {
+  browserSync.init(
+    null,
+    {
+      proxy: 'http://localhost:3000',
+      files: ['dev/**/*.*'],
+      port: 5000
+    },
+    done
+  );
+}
+
+function watchTask() {
+  watch('dev/scss/**/*.scss', sassTranslate);
+  watch('dev/**/*.pug', pugTranslate);
+}
+
+// gulp.task('watch', () => {
+//   gulp.watch('dev/scss/**/*.scss', gulp.parallel('sass'));
+//   gulp.watch('dev/**/*.pug', gulp.parallel('pug'));
+// });
+
+// gulp.task('default', gulp.parallel('sass', 'pug', 'watch')});
+
+module.exports.default = series(
+  // parallel(sassTranslate, pugTranslate),
+
+  parallel(sassTranslate, pugTranslate),
+  nodemonTask,
+  browserSyncTask,
+  watchTask
+);
